a0027da9ebc0c00709535de7fd2ec79098471dbd
Aniruddh Dawale <148671858+Annniruddh@users.noreply.github.com>
2024-11-06 06:23:48 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 2425011
+let accountEmail = "Aniruddh03@gmail.com"
+var accountPassword = "0164"
+accountCity = "Mumbai"
+let accountState;
+
+
+// accountId = 2
+
+// {
+//     This are known as Scope
+// }
+
+accountEmail = "asd@gmail.com"
+accountPassword = "0816"
+accountCity = "Manchester"
+
+console.log(accountId);
+
+/*
+This is a new type of comment,
+also prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version as it is also not needed because machine automatically treats it as new version of JS
+// alert(3 + 3); // We are usimg NodeJS, not browser
+
+console.log(3 + 3);
+
+//code readability should be high write code in such a way that it should be easily readable and understanable.
+
+console.log("Aniruddh")
+
+
+
+let name = "Aniruddh"
+let age = 18
+let isloggedIn = true
+let state;
+
+
+// number ==> 2 to the power 53 also we have Bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> stand alone value, a type, can be assigned as null
+// undefined ==> no value has been assigned, is a type itself
+//symbol ==> to find uniqeness between objects and all
+
+//object 
+
+console.log(typeof null); //type : object
+console.log(typeof undefined); //type : undefined
