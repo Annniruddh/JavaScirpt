f013e7eafc031962d862d242f9e9ad6d5e73bfb9
Aniruddh Dawale <148671858+Annniruddh@users.noreply.github.com>
2024-11-06 12:07:31 +0000

    Conversion Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,12 +30,13 @@ let stringNumber = String(someNumber)
 // *********************** Operations ***********************
 
 let value = 3
-let negValue = -value
-// console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3)
