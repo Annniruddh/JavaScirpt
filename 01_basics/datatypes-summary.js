d17195495eb76f9117301bb00d437ebf16deef53
Aniruddh Dawale <148671858+Annniruddh@users.noreply.github.com>
2024-11-26 06:40:56 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// Primitive Datatypes - 7 Categories
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const score_value = 200
+
+const isloggedin = true
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+
+// Reference Datatype (Non-Primitive)
+// Arrays, Objects, Functions
