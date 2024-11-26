0d701f49bf171c94174f9e4ad07ce3476590f362
Aniruddh Dawale <148671858+Annniruddh@users.noreply.github.com>
2024-11-26 06:08:04 +0000

    container config done

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,16 @@
+// Clean code has the most value 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Avoid above type of code 
+
+
+// Strictness of comparison
+console.log("2" == 2);
+console.log("2" === 2);
